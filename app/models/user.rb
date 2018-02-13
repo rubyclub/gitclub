41477bb5fb1c0fbe6e831c464fe107fad723ad9b
Kirill Kovalenko <kavalenka@1pt.com>
2018-02-13 19:45:34 +0300

    Code refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,17 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
-  before_save { self.email = email.downcase }
-  validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+  has_secure_password
+
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name,  presence: true, uniqueness: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
             format: { with: VALID_EMAIL_REGEX },
             uniqueness: { case_sensitive: false }
-  has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
+
+  before_save { self.email = email.downcase }
+
+  attr_accessor :remember_token
 
   # Returns the hash digest of the given string.
   def User.digest(string)

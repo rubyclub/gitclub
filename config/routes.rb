36ebd1f3a6c3487549bfcee9f78bf42cfe8fd26f
andreydro <andreydro14@gmail.com>
2018-02-14 18:53:39 +0300

    Add-user-logged-in-page
    Update-dashboard-page
    
    Add profile page
    
    Update-profile-page-and-fix-scss-files
    
    Update logged_in method in _top.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get '/forks', to: 'insights#forks'
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
+  get '/profile', to: 'users#profile'
   get '/login', to:'sessions#new'
   post '/login', to:'sessions#create'
   delete '/logout', to:'sessions#destroy'
